be382c43d2eac9ad298fff6104c294b5ca10577b
zrvnarmarin <76marin76@gmail.com>
2022-11-24 02:47:10 +0100

    styling home page

--- filip-rade-photography/src/components/Home.jsx
+++ filip-rade-photography/src/components/Home.jsx
@@ -1,10 +1,65 @@
 import React from 'react'
+import TestImage from '../assets/gallery5.jpg'
 
 const Home = () => {
   return (
-    <div className='bg-slate-700'>
-      home
+   
+    <div className='body-gradient'>
+      <div className="container mx-auto items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="w-full">
+          <img
+            src="https://images.unsplash.com/photo-1432457990754-c8b5f21448de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
+            alt="Photo by Claudio Schwarz on Unsplash"
+            className="inset-0 h-full w-full object-cover object-center  opacity-75 hover:opacity-100" />
+        </div>
+        <div className="w-full col-span-2">
+          <img
+            src="https://images.unsplash.com/photo-1586921829167-409624a3734a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Photo by Claudio Schwarz on Unsplash"
+            className="inset-0 h-full w-full object-cover object-center  opacity-75 hover:opacity-100 " />
+        </div>
+        <div className="w-full ">
+          <img
+            src="https://images.unsplash.com/photo-1489981424607-45c58daf0581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Photo by Claudio Schwarz on Unsplash"
+            className="inset-0 h-full w-full object-cover object-center  opacity-75 hover:opacity-100 " />
+        </div>
+        <div className="w-full">
+          <img
+            src="https://images.unsplash.com/photo-1489981424607-45c58daf0581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Photo by Claudio Schwarz on Unsplash"
+            className="inset-0 h-full w-full object-cover object-center  opacity-75 hover:opacity-100 " />
+        </div>
+        <div className="w-full ">
+          <img
+            src="https://images.unsplash.com/photo-1586921829167-409624a3734a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Photo by Claudio Schwarz on Unsplash"
+            className="inset-0 h-full w-full object-cover object-center  opacity-75 hover:opacity-100 " />
+        </div>
+      
+        <div className="w-full ">
+          <img
+            src="https://images.unsplash.com/photo-1534531173927-aeb928d54385?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Photo by Claudio Schwarz on Unsplash"
+            className="inset-0 h-full w-full object-cover object-center  opacity-75 hover:opacity-100 " />
+        </div>
+        <div className="w-full">
+          <img
+            src="https://images.unsplash.com/photo-1489981424607-45c58daf0581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Photo by Claudio Schwarz on Unsplash"
+            className="inset-0 h-full w-full object-cover object-center  opacity-75 hover:opacity-100 " />
+        </div>
+        <div className="w-full">
+          <img
+            src="https://images.unsplash.com/photo-1489981424607-45c58daf0581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Photo by Claudio Schwarz on Unsplash"
+            className="inset-0 h-full w-full object-cover object-center  opacity-75 hover:opacity-100 " />
+        </div>
+      </div>
+        </div>
     </div>
+  
   )
 }
 
