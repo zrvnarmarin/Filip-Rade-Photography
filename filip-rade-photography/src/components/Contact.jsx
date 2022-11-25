53bee62a31470d09009de93dfd7130e189fae0fc
zrvnarmarin <76marin76@gmail.com>
2022-11-25 02:50:14 +0100

    contact section finished

--- filip-rade-photography/src/components/Contact.jsx
+++ filip-rade-photography/src/components/Contact.jsx
@@ -1,63 +1,67 @@
 import React from 'react'
+import ContactImage from '../assets/contactMe.png'
+import { contactInfo } from '../data/contacts'
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <div className='grid grod-cols-1 sm:grid-cols-2 px-12'>
-        <img className='place-self-center' src='https://www.pngkit.com/png/full/235-2355157_there-was-a-problem-submitting-your-form-contact.png' />
+    <div className='font-robotoSlab bg-[#1f1f21]'>
+      <h1 className='text-center text-white font-light text-6xl pt-8 pb-14'>Contact</h1>
+
+      <div className='grid grod-cols-1 sm:grid-cols-2 xs:px-4 sm:px-12 gap-6'>
+        <img className='place-self-center' src={ContactImage} />
         
-        <div className=''>
-          <h2 className='bg-slate-400 p-2 flex flex-wrap'>For all enquires and bookings, please contact me using one of the
+        <div>
+          <h2 className='p-2 flex flex-wrap text-3xl text-white font-medium'>Will you make your memories count?</h2>
+          <h2 className='p-2 flex flex-wrap text-white text-lg font-normal italic'>For all enquires and bookings, please contact me using one of the
             following, or the form below..
           </h2>
-          <div className='bg-slate-500'>
+          <div className='pt-4 pb-4'>
             <div className='px-2 py-1'>
-              <span>Email: </span>
-              <a href=''>76marin76@gmail.com</a>
+              <span className='text-white text-lg font-medium'>Email: </span>
+              <a className='text-slate-300 hover:text-slate-900 hover text-base' href=''>{contactInfo.email}</a>
             </div>
             <div className='px-2 py-1'>
-              <span>Facebook: </span>
-              <a href=''>facebook.com</a>
+              <span className='text-white text-lg font-medium'>Facebook: </span>
+              <a className='text-slate-300 hover:text-slate-900 text-base' href={contactInfo.facebook}>{contactInfo.facebook}</a>
             </div>
             <div className='px-2 py-1'>
-              <span>Instagram: </span>
-              <a href=''>instagram.com</a>
+              <span className='text-white text-lg font-medium'>Instagram: </span>
+              <a className='text-slate-300 hover:text-slate-900 text-base' href={contactInfo.instagram}>{contactInfo.instagram}</a>
             </div>
             <div className='px-2 py-1'>
-              <span>Phone: </span>
-              <a href=''>76marin76@gmail.com</a>
+              <span className='text-white text-lg font-medium'>Phone: </span>
+              <a className='text-slate-300 hover:text-slate-900 text-base' href=''>{contactInfo.phone}</a>
             </div>
           </div>
 
-          <form className='border-2 border-black bg-slate-200'>
+          <form className='pt-2 border-black'>
 
-            <div className='bg-slate-400'>
-              <label className='px-2 py-1' htmlFor="nameInput">Name*</label> <br />
-              <div className='flex flex-wrap sm:flex-row gap-2 px-2 py-1'>
+            <div className='pb-4'>
+              <label className='px-2 py-1 text-white' htmlFor="nameInput">Name*</label> <br />
+              <div className='flex flex-wrap sm:flex-row gap-4 px-2 py-1'>
                 <div className='flex flex-col flex-1'>
-                  <input className='flex flex-1'  type="text" />
-                  <label>First name</label>
+                  <input className='p-3 flex flex-1 focus:outline-none' type="text" />
+                  <span className='text-sm text-slate-100 pt-1'>First name</span>
                 </div>
                 <div className='flex flex-col flex-1'>
-                  <input className='flex flex-1'  type="text" />
-                  <label>Last name</label>
+                  <input className='p-3 flex flex-1 focus:outline-none'  type="text" />
+                  <span className='text-sm text-slate-100 pt-1'>Last name</span>
                 </div>
               </div>
             </div>
 
-            <div className='bg-slate-500 flex flex-col'>
-              <label className='px-2 py-1' htmlFor="emailInput">Email*</label> <br />
-              <input className='flex flex-1 mx-2' type="text" />
+            <div className='flex flex-col gap-1 pb-4'>
+              <label className='px-2 py-1 text-white' htmlFor="emailInput">Email*</label> 
+              <input className='flex flex-1 mx-2 p-3 focus:outline-none' type="text" />
             </div>
 
-            <div className='bg-slate-600 flex flex-col'>
-              <label className='px-2 py-1' htmlFor="emailInput">Message*</label> <br />
-              <textarea className='flex flex-1 mx-2' name="" id="" cols="30" rows="10"></textarea>
+            <div className='flex flex-col gap-1'>
+              <label className='px-2 py-1 text-white' htmlFor="emailInput">Message*</label> 
+              <textarea className='flex flex-1 mx-2 p-3 resize-y focus:outline-none' name="message" id="messageInput" cols="30" rows="10"></textarea>
             </div>
 
-            <div className='bg-slate-700 px-2 py-7'>
-              <button className='px-12 py-6 bg-slate-900 text-white'>Submit</button>
+            <div className='px-2 py-7'>
+              <button className='px-12 py-6 bg-[#494949] text-white'>Submit</button>
             </div>
 
           </form>
---
+++ filip-rade-photography/src/data/contacts.js
@@ -0,0 +1,6 @@
+export const contactInfo = {
+    email: 'filiprade@gmail.com',
+    facebook: 'https://www.facebook.com/filip.rade',
+    instagram: 'https://www.instagram.com/filiprade',
+    phone: '+385989504100'
+}
