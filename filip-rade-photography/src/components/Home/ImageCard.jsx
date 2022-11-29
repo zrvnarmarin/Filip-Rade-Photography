33e3b12e99071c9a539412a2ed33a29c22427ebd
zrvnarmarin <76marin76@gmail.com>
2022-11-29 02:22:44 +0100

    gallery modal finished

--- filip-rade-photography/src/components/Home/ImageCard.jsx
+++ filip-rade-photography/src/components/Home/ImageCard.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import '../../styles/ImageCard.css'
 
-const ImageCard = ({name, source, onOpenModal}) => {
+const ImageCard = ({id, name, source, onOpenModal, onShowImageObject}) => {
+
+  const openModalAndInfo = (e) => {
+    onOpenModal()
+
+    const imageObjectData = {
+      name: name,
+      id: id,
+      source: source
+    }
+
+    onShowImageObject(imageObjectData)
+  }
   return (
     <div className="container">
-        <img src={source} className="image" onClick={onOpenModal} />
+        <img src={source} className="image" onClick={openModalAndInfo} />
         <div className="middle">
           <div className="text-white ">+</div>
         </div>
--- filip-rade-photography/src/components/Projects.jsx
+++ filip-rade-photography/src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, { useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ImageCardsGrid from './Home/ImageCardsGrid'
 import ImageCard from './Home/ImageCard'
@@ -7,44 +7,47 @@ import '../styles/Home.css'
 import TestImage from '../assets/nature.jpg'
 
 const Projects = () => {
+  const [imageObject, setImageObject] = useState({})
   const { imageCategory } = useParams()
   const modalRef = useRef()
+
   const openModalHandler = () => modalRef.current.style.display = "block"
   const closeModalHandler = () => modalRef.current.style.display = "none"
+
   const sum = imageCategories.map(category => { if (category.name === imageCategory) return category.images.length })
-  const showImageId = (id) => {
-    imageCategories.map(category => {
-      category.images.find(image => image.id === id)
-    })
+
+  const displayImageObject = (imageObject) => {
+    console.log(imageObject)
+    setImageObject(imageObject)
   }
-   
-  
 
   return (
     <>
       <ImageCardsGrid>
         {imageCategories.map(category => {
-          // console.log(category.name === imageCategory ? 'true' : 'false')
           if (category.name === imageCategory) {
             return category.images.map(image => 
-              <ImageCard key={image.id} name={image.name} source={image.source} onOpenModal={openModalHandler} />
+              <ImageCard 
+                key={image.id} 
+                id={image.id} 
+                name={image.name} 
+                source={image.source} 
+                onOpenModal={openModalHandler}
+                onShowImageObject={displayImageObject}
+              />
             )}
           }
         )}
         
         <div className="modal" ref={modalRef}>
-          <span className='slide-number'>8/{sum}</span>
+          <span className='slide-number'>{imageObject.id}/{sum}</span>
           <span className="close cursor" onClick={closeModalHandler} >&times;</span>
           <div className="modal-content">
-
-            {imageCategories.map(category => {
-              if (category.name === imageCategory) {
-                return category.images.map(image => 
-                  category.images.find(image => image.id === id)
-                  // <img key={image.id} src={image.source} onClick={showImageId} />
-                )}
-            })}
-            {/* <img src={TestImage} onClick={showImageId} /> */}
+            <div>
+              <p>{imageObject.id}</p>
+              <p>{imageObject.id}</p>
+              <img src={imageObject.source} />
+            </div>
             
             <a className="prev" >&#10094;</a>
             <a className="next" >&#10095;</a>
