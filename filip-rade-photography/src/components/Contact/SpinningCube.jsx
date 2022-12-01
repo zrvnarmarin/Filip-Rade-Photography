8a66f2da5f9c81e8ca7da7975f951da3c06f444d
zrvnarmarin <76marin76@gmail.com>
2022-12-01 23:30:35 +0100

    cleanup

--- filip-rade-photography/src/components/Contact/SpinningCube.jsx
+++ filip-rade-photography/src/components/Contact/SpinningCube.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { socialMediaImages } from '../../data/socialMediaImages'
+import { contactInfo } from '../../data/contacts'
 
 const SpinningCube = () => {
   return (
@@ -7,22 +8,22 @@ const SpinningCube = () => {
         <div className="box-area">
         <div className="box box-front">
             <div className="test-container">
-            <a href="https://www.linkedin.com"><img className="test-image" src={socialMediaImages.linkedin} /></a>
+            <a href={contactInfo.linkedin}><img className="test-image" src={socialMediaImages.linkedin} /></a>
             </div>
         </div>
         <div className="box box-right">
             <div className="test-container">
-            <a href="https://www.facebook.com"><img className="test-image" src={socialMediaImages.facebook} /></a>
+            <a href={contactInfo.facebook}><img className="test-image" src={socialMediaImages.facebook} /></a>
             </div>
         </div>
         <div className="box box-back">
             <div className="test-container">
-            <a href="https://www.instagram.com"><img className="test-image" src={socialMediaImages.instagram} /></a>
+            <a href={contactInfo.instagram}><img className="test-image" src={socialMediaImages.instagram} /></a>
             </div>
         </div>
         <div className="box box-left">
             <div className="test-container">
-            <a href="https://www.vero.com"><img className="vero-image" src={socialMediaImages.vero} /></a>
+            <a href={contactInfo.vero}><img className="vero-image" src={socialMediaImages.vero} /></a>
             </div>
         </div>
         <div className="box box-top"></div>
