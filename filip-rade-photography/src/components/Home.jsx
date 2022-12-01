e7cb2bb217bf285beae0676813fe1e42aa8e0e31
zrvnarmarin <76marin76@gmail.com>
2022-12-01 10:37:33 +0100

    home page done

--- filip-rade-photography/src/components/Home.jsx
+++ filip-rade-photography/src/components/Home.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
+import React, {useState, useRef } from 'react'
+import ImageCardsGrid from './Projects/ImageCardsGrid'
+import ImageCard from './Projects/ImageCard.jsx'
+import ImageModal from './Projects/ImageModal'
+import { mainSeriesCategory } from '../data/imageCategories'
 
 const Home = () => {
-  
+  const [currentImageObject, setCurrentImageObject] = useState({})
+  const modalRef = useRef()
+
+  const openModalHandler = () => modalRef.current.style.display = "block"
+  const closeModalHandler = () => modalRef.current.style.display = "none"
+
+  const numberOfImages = mainSeriesCategory.map(category => category.images.length)
+
+  const setNextImageObject = () => {
+    mainSeriesCategory.map(category => {
+      const nextImageObject = category.images.find(categoryImageObject => categoryImageObject.id === currentImageObject.id + 1)
+      setCurrentImageObject(nextImageObject)
+
+      if (nextImageObject === undefined) 
+        setCurrentImageObject(category.images[0])
+    })
+  }
+
+  const setPreviousImageObject = () => {
+    mainSeriesCategory.map(category => {
+      const previousImageObject = category.images.find(categoryImageObject => categoryImageObject.id === currentImageObject.id - 1)
+      setCurrentImageObject(previousImageObject)
+      
+      if (previousImageObject === undefined) 
+        setCurrentImageObject(category.images[category.images.length - 1])
+    })
+  }
   
   return (
-    <>
-      Home
-    </>
+    <ImageCardsGrid>
+      {mainSeriesCategory.map(category => {
+        return category.images.map(image => 
+          <ImageCard 
+            key={image.id} 
+            id={image.id} 
+            name={image.name} 
+            source={image.source} 
+            description={image.description}
+            onOpenModal={openModalHandler}
+            onSetImageObject={setCurrentImageObject}
+          />
+        )}
+      )}
+
+      <ImageModal
+        modalRef={modalRef} 
+        numberOfImages={numberOfImages}  
+        currentImageId={currentImageObject.id}
+        currentImageSource={currentImageObject.source}
+        onCloseModal={closeModalHandler}
+        onSetPreviousImageObject={setPreviousImageObject}
+        onSetNextImageObject={setNextImageObject}
+      />
+    </ImageCardsGrid>
   )
 }
 
--- filip-rade-photography/src/data/imageCategories.js
+++ filip-rade-photography/src/data/imageCategories.js
@@ -19,3 +19,16 @@ export const imageCategories = [
         ]
     }
 ]
+
+export const mainSeriesCategory = [
+    {
+        id: 1, name: 'Urban Series', description: 'This is an urban category of images', 
+        images: [
+            {id: 1, name: 'zgrada', description: 'Zgrada jedan', source: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNgQLhV4izhvr6NW2l_xqCU4_fd3I7Y-3g6GJvO8jEhFa3nTcZyKi8BrESg4Qb0md4Yu0&usqp=CAU'},
+            {id: 2, name: 'neka zgrada', description: 'Zgrada dva', source: 'https://img.andrewprokos.com/PRINCETON-ROCKEFELLER-COLLEGE-CLOISTER-0777-BW-1000PX.jpg'},
+            {id: 3, name: 'tamo  zgrada', description: 'Zgrada tri', source: 'https://c8.alamy.com/comp/C54PYX/paris-interior-of-saint-severin-gothic-church-C54PYX.jpg'},
+            {id: 4, name: 'opet zgrada', description: 'Zgrada cetiri', source: 'https://st2.depositphotos.com/1444936/11889/i/950/depositphotos_118898722-stock-photo-interior-gothic-architecture-detail.jpg'},
+            {id: 5, name: 'tamo neka zgrada', description: 'Zgrada pet', source: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNgQLhV4izhvr6NW2l_xqCU4_fd3I7Y-3g6GJvO8jEhFa3nTcZyKi8BrESg4Qb0md4Yu0&usqp=CAU'}
+        ]
+    }
+]
